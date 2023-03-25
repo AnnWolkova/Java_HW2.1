0fc7ed679cb1a6ea03fbc74268957405f9ecf187
AnnWolkova <volkov.nikita.a@gmai.com>
2023-03-25 17:56:44 +0700

    change all

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+public class Main {
+    public static void main(String[] args) {
+        int priceTiket=20000;                           // стоимости билета
+        int oneBonusMile=20;                            // количество рублей для одной бонусной мили
+
+        int bonusMiles=  priceTiket/oneBonusMile;   //  количество бонусных миль
+        System.out.println("Кол-во бонусных миль: "+ bonusMiles);   // Вывод ответа
+    }}
